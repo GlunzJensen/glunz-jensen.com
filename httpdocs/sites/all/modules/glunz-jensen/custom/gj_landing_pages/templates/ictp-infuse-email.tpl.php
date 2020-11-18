83433b50e01269e8dd980648f3badf0f431a71c7
Andriy Iun <andriy.yun@gmail.com>
2020-11-18 09:51:31 +0100

    OGQ-791-42370 Added custom template to Plate writer printer. Added sass file for new landing

---
+++ httpdocs/sites/all/modules/glunz-jensen/custom/gj_landing_pages/templates/ictp-infuse-email.tpl.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @file Overrinden page tpl file for Plate writer printer
+ */
+?>
+<p>Hi</p>
+<p>There has been made request for more information about iCtP TM Infuse-kit on page <?php print $page; ?></p>
+<p>Request details:</p>
+<ul>
+  <li>Name: <?php print $name; ?></li>
+  <li>Email: <?php print $email; ?></li>
+  <li>Company: <?php print $company; ?></li>
+  <li>Country: <?php print $country; ?></li>
+</ul>
